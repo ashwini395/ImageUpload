c4746132197775071cef86857fdf3e0071993bfe
ashwini395 <76863343+ashwini395@users.noreply.github.com>
2021-06-26 15:42:40 +0530

    Update index.js
    change

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/upload', upload.single('photo'), async (req, res) => {
                     <h1 class="card-header">Image Uploaded successfully</h1>
                 </div>
                 <div style="width:100%; height:auto">
-                  <img style="width:100%; height:auto" src=${imagaPath} alt="">
+                  <img style="width:50%; height:auto" src=${imagaPath} alt="">
                 </div>
             </div>
 
